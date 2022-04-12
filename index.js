b5d71d88ccc6a1bfcd20920a6941f518bfd03500
Bruce Hsu <brucehsu1126@gmail.com>
2022-04-12 11:29:12 -0700

    added form

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ function Book(author, title, numPages, read, review) {
 function addBookToLibrary() {
 
 }
+
+const addBookBtn = document.getElementById('add-book-btn');
+const modalContainer = document.getElementById('modal-container')
+const closeBtn = document.getElementById('close-button');
+
+
+addBookBtn.addEventListener('click', function () {
+    modalContainer.classList.add('show');
+});
+
+closeBtn.addEventListener('click', function () {
+    modalContainer.classList.remove('show');
+});
