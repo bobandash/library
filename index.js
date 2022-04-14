25fe9f1afd598f79d1fcdf0ced780657e2fd5923
bobandash <brucehsu1126@gmail.com>
2022-04-14 16:37:12 -0700

    fixed the add books functionality

--- index.js
+++ index.js
@@ -15,39 +15,41 @@ function Book(title, author, numPages, read) {
 }
 
 function addBookToLibrary() {
+    const allBooksContainer = document.getElementById('all-books-container');
     createNewBook();
+    displayBook(allBooksContainer, myLibrary.length - 1);
 }
 
 function createNewBook() {
-    const allBooksContainer = document.getElementById('all-books-container');
-    const bookName = document.getElementById('book-name').value;
-    const authorName = document.getElementById('author-name').value;
-    const numPages = document.getElementById('num-pages').value;
-    const bookRead = document.getElementById("book-read").value;
-    let newBook =  Book(bookName, authorName, numPages, bookRead);
+    let bookName = document.getElementById('book-name').value;
+    let authorName = document.getElementById('author-name').value;
+    let numPages = document.getElementById('num-pages').value;
+    let bookRead = document.getElementById("book-read").value;
+    let newBook =  new Book(bookName, authorName, numPages, bookRead);
     myLibrary.push(newBook);
-    displayBook(allBooksContainer, myLibrary.length - 1)
 }
 
 function displayBook(allBooksContainer, index) {
-    const numCategories = 4;
-    const numButtons = 3;
 
     let bookContainer = document.createElement('div');
-    let textCategory = document.createElement('div');
-    let textContent = document.createElement('div');
-    let btnsContent = document.createElement('div');
-
     allBooksContainer.appendChild(bookContainer);
-    bookContainer.appendChild(textCategory);
-    bookContainer.appendChild(textContent);
-    bookContainer.appendChild(btnsContent);
-
     bookContainer.classList.add('books-container');
-    textCategory.classList.add('text-category');
-    textContent.classList.add('text-content');
-    btnsContent.classList.add('buttons-content');
 
+    addCategoryTextAndValues(bookContainer, index);
+    addButtons(bookContainer, index);
+}
+
+
+function addCategoryTextAndValues(bookContainer, index) {
+    const numCategories = 4;
+
+    let textCategory = document.createElement('div');
+    bookContainer.appendChild(textCategory);
+    textCategory.classList.add('text-category');
+
+    let textContent = document.createElement('div');
+    bookContainer.appendChild(textContent);
+    textContent.classList.add('text-content');
     for(let i = 0; i < numCategories; i++) {
         let categoryName = document.createElement('div');
         let categoryValue = document.createElement('div');
@@ -75,26 +77,35 @@ function displayBook(allBooksContainer, index) {
                 break;
         }
     }
+}
 
+function addButtons(bookContainer, index) {
+    const numButtons = 3;
+    let btnsContent = document.createElement('div');
+    bookContainer.appendChild(btnsContent);
+    btnsContent.classList.add('buttons-content');
     for(let i = 0; i < numButtons; i++) {
-        let currButton = document.createElement('div');
+        let currButton = document.createElement('button');
         let currIcon = document.createElement('i');
         btnsContent.appendChild(currButton);
         currButton.appendChild(currIcon);
         currIcon.classList.add('fa-solid');
         switch (i) {
             case 0:
+                currButton.classList.add('delete-btn');
                 currIcon.classList.add('fa-trash');
+/*                  currButton.addEventListener('click', removeBook(index)); */
                 break;
             case 1:
+                currButton.classList.add('edit-btn');
                 currIcon.classList.add('fa-pen');
                 break;
             case 2:
+                currButton.classList.add('toggle-read-btn');
                 currIcon.classList.add('fa-book');
                 break;
         }
     }
-
 }
 
 function displayAllBooks() {
@@ -104,8 +115,32 @@ function displayAllBooks() {
     }
 }
 
+function removeBook(index) {
+    myLibrary.splice(index,1);
+    clearDisplay();
+    displayAllBooks();
+}
 
-/* formSubmitBtn.addEventListener('click', addBookToLibrary) */
+function clearDisplay() {
+    let allBooksContainer = document.getElementById('all-books-container');
+    let allBooks = Array.from(document.querySelectorAll('.books-container'));
+    for(let i = 0; i < allBooks.length; i++)
+    {
+        allBooksContainer.removeChild(allBooks[i]);
+    }
+}
+
+//the form's default reaction is to refresh the page,
+//this prevents the form from refreshing the page
+let form = document.getElementById("add-book-form");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+
+//form functionality
+formSubmitBtn.addEventListener('click', function() {
+    addBookToLibrary();
+    modalContainer.classList.remove('show');
+})
 
 addBookBtn.addEventListener('click', function () {
     modalContainer.classList.add('show');
@@ -116,6 +151,6 @@ closeBtn.addEventListener('click', function () {
 });
 
 
-
 //main function
 displayAllBooks();
+//the issue is that the form is editing the url
